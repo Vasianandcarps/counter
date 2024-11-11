ec603c47532ebfe80941628645476612b1af6de2
Anna <anna.2005.nikulina@gmail.com>
2024-11-11 14:55:40 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/counter-of-experience/',
+   base: '/counter/',
   plugins: [
     vue(),
   ],
